f877fad1d88a4463aece67b49435c7cb22e77cfa
Gabriel Bolbotina <80618569+gabriel-bolbotina@users.noreply.github.com>
2022-04-15 15:13:14 +0300

    New repo done

---
+++ lib/SecondPage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+
+  const SecondPage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+
+  @override
+  State<StatefulWidget> createState()  => _SecondPageState();
+
+
+}
+
+  class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("This is the second page"),
+    ),
+    body: Center(
+       child: Text("Welcome to the second page!"),
+  ),
+
+
+
+
+    );
+  }
+  }
+
+
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_away1/SecondPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login page',
+          style: TextStyle(
+          color: Colors.amberAccent,
+            fontStyle: FontStyle.italic,
+        ),
+        ),
+      backgroundColor: Colors.purple,
+      foregroundColor: Colors.purpleAccent,
+
+
+
+
+      ),
+
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+
+
+            Text(
+              'Go to the other page',
+            style: TextStyle(
+                color: Colors.purple,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+                fontSize: 25)
+              ,
+            ),
+
+
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondPage(title: 'title')),
+            );
+          },
+          backgroundColor: Colors.amberAccent,
+      ),
+
+    );
+
+  }
+}
